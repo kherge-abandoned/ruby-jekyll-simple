bf74254709941b796b5a28b57535e21bc9f198d8
Kevin Herrera <kevin@herrera.io>
2013-06-06 15:02:51 -0700

    Adding tag archive.

--- assets/js/simple.js
+++ assets/js/simple.js
@@ -1,3 +1,22 @@
+// http://stackoverflow.com/a/3855394
+(function ($) {
+    $.queryString = (function (a) {
+        var i,
+            p,
+            b = {};
+        if (a === "") {
+            return {};
+        }
+        for (i = 0; i < a.length; i += 1) {
+            p = a[i].split('=');
+            if (p.length === 2) {
+                b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+            }
+        }
+        return b;
+    }(window.location.search.substr(1).split('&')));
+}(jQuery));
+
 $(function () {
     var carouselCounter = 0;
 
